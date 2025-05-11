997e4b99fd5714b643b0921e86596e0374325e28
horacexd <horace.d@outlook.com>
2025-05-10 17:57:45 -0700

    feat: add echo func

---
+++ app/handler.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"net"
+	"strconv"
+	"fmt"
+)
+
+// assume receive byte string e.g.: +PING\r\n
+func parseSimpleString(buf []byte, pos *int) []string {
+	tokens := []string{}
+	s := *pos
+	for *pos < len(buf) - 1 {
+		if buf[*pos] == '\r' && buf[*pos + 1] == '\n' {
+			break
+		}
+		*pos++
+	}
+	
+	tokens = append(tokens, string(buf[s:*pos]))
+	return tokens
+}
+
+func parseAndExecuteSimpleError(conn net.Conn, buf []byte, pos *int) error {
+	// Parse the simple error from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteInteger(conn net.Conn, buf []byte, pos *int) error {
+
+	// Parse the integer from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseBulkString(buf []byte, pos *int) []string {
+	// Parse the bulk string from the buffer and send it to the connection
+	// Implementation goes here
+	tokens := []string{}
+	s := *pos
+	l := getLength(buf, pos)
+	// fmt.Println("[DEBUG] slice=[%x]", buf[s:s + l])
+	tokens = append(tokens, string(buf[s:s + l]))
+	*pos += (l + 2)
+	// fmt.Println("[DEBUG] bulk string len=[%d] val=[%s]", l, tokens)
+	return tokens
+}
+
+func parseArray(buf []byte, pos *int) []string {
+	// Parse the array from the buffer and send it to the connection
+	// Implementation goes here
+	tokens := []string{}
+	// get length of the array
+	l := getLength(buf, pos)
+	if l < 1 {
+		return tokens
+	}
+
+	// get token of each element in the array
+	for i := 0; i < l; i++ {
+		switch buf[*pos] {
+		case '$':
+			*pos++
+			tmp := parseBulkString(buf, pos)
+			fmt.Println("[DEBUG] get bulkstring=%s", tmp)
+			tokens = append(tokens, tmp...)
+		}
+	}
+	// debug 
+	// fmt.Println("[DEBUG] array=%s", tokens)
+	return tokens
+}
+
+func parseAndExecuteNull(conn net.Conn, buf []byte, pos int) error {
+	// Parse the null from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteDouble(conn net.Conn, buf []byte, pos int) error {
+	// Parse the double from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteBigNumbers(conn net.Conn, buf []byte, pos int) error {
+	// Parse the big numbers from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteBulkErrors(conn net.Conn, buf []byte, pos int) error {
+	// Parse the bulk errors from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteBoolean(conn net.Conn, buf []byte, pos int) error {
+	// Parse the boolean from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteVerbatimStrings(conn net.Conn, buf []byte, pos int) error {
+	return nil
+}
+
+func parseAndExecuteMaps(conn net.Conn, buf []byte, pos int) error {
+	// Parse the maps from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteAttributes(conn net.Conn, buf []byte, pos int) error {
+	// Parse the attributes from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecuteSets(conn net.Conn, buf []byte, pos int) error {
+	// Parse the sets from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+func parseAndExecutePushes(conn net.Conn, buf []byte, pos int) error {
+	// Parse the pushes from the buffer and send it to the connection
+	// Implementation goes here
+	return nil
+}
+
+
+// HELPER FUNCTION
+func getLength(buf []byte, pos *int) int {
+	s := *pos
+	fmt.Println("[DEBUG] getLength start=%d", *pos)
+	for *pos < len(buf) - 1 {
+		if buf[*pos] == '\r' && buf[*pos + 1] == '\n' {
+			break
+		}
+		*pos++
+	}
+	l, _ := strconv.Atoi(string(buf[s:*pos]))
+	*pos += 2
+	fmt.Println("[DEBUG] getLength end=%d", *pos)
+
+	return l
+} 
--- app/main.go
+++ app/main.go
@@ -5,11 +5,13 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+const (
+	MAX_CONNECTIONS = 1000
+	MAX_CONNECTION_TIMEOUT = 10 // seconds
+)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -24,14 +26,19 @@ func main() {
 	}
 	defer l.Close()
 	// we may have multiple connections
+	
+	// for i := 0; i < MAX_CONNECTIONS; i++ {
 	for {
+		// TODO: how to restrict max munber of connections?
+		// TODO: how to set up timeout for connections?
+		// TODO: for long running connections, how to set up?
 		go handleConnection(l)
 	}
 }
 
 func handleConnection(l net.Listener) {
 	conn, err := l.Accept()
-	defer conn.Close()
+	// defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
@@ -43,16 +50,143 @@ func handleConnection(l net.Listener) {
 		if err != nil {
 			if err == io.EOF {
 				continue
-			} else {
-				fmt.Println("Error reading: ", err.Error())
-			}
+			} 
+			fmt.Println("Error reading: ", err.Error())
 		}
-		// parse command 
-		parseAndexecuteCommand(conn, buf)
-		// conn.Write([]byte("+PONG\r\n"))
+		// parse and execute command 
+		if err := parseAndExecuteCommand(conn, buf); err != nil {
+			fmt.Println("Error parsing command: ", err.Error())
+			continue
+		}
 	}
 }
 
-func parseAndexecuteCommand(conn net.Conn, buf []byte) {
-	
+func parseAndExecuteCommand(conn net.Conn, buf []byte) error {
+	if len(buf) == 0 {
+		return nil
+	}
+	// TODO(LOW): change it could run multiple commands in one statement
+	// convert byte array to string
+	pos := 0
+	var tokens []string
+	for pos < len(buf) {
+		if buf[pos] == '\x00' {
+			break
+		}
+		switch buf[pos] {
+		case '+':
+			// simple string
+			pos++
+			tokens = parseSimpleString(buf, &pos)
+			fmt.Println("[DEBUG] simplestring=[%s]", tokens)
+		// case '-':
+		// 	// simple error
+		// 	if err := parseAndExecuteSimpleError(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case ':':
+		// 	// integer
+		// 	if err := parseAndExecuteInteger(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		case '$':
+			// bulk strings
+			pos++
+			tokens = parseBulkString(buf, &pos)
+			fmt.Println("[DEBUG] bulkstring=[%s]", tokens)
+		case '*':
+			// array
+			pos++
+			fmt.Println("[DEBUG] receive array. buf=%s pos=%d", buf, pos)
+			tokens = parseArray(buf, &pos)
+			fmt.Println("[DEBUG] array=[%s]", tokens)
+		// case '_':
+		// 	// null
+		// 	if err := parseAndExecuteNull(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '#':
+		// 	// boolean
+		// 	if err := parseAndExecuteBoolean(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case ',':
+		// 	// double
+		// 	if err := parseAndExecuteDouble(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '(':
+		// 	// big numbers
+		// 	if err := parseAndExecuteBigNumbers(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '!':
+		// 	// bulk errors
+		// 	if err := parseAndExecuteBulkErrors(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '=':
+		// 	// verbatim strings
+		// 	if err := parseAndExecuteVerbatimStrings(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '%':
+		// 	// maps
+		// 	if err := parseAndExecuteMaps(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '|':
+		// 	// attributes
+		// 	if err := parseAndExecuteAttributes(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '~':
+		// 	// sets
+		// 	if err := parseAndExecuteSets(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+		// case '>':
+		// 	// pushes
+		// 	if err := parseAndExecutePushes(conn, buf, &pos); err != nil {
+		// 		return err
+		// 	}
+
+		default:
+			// unknown command
+			fmt.Println("[DEBUG] current pos=%d", pos)
+			return fmt.Errorf("unknown command: %v", buf[pos])
+		}
+	}
+
+	fmt.Println("[DEBUG] token=[%s]", tokens)
+	execute(conn, tokens)
+	return nil
 }
+
+func execute(conn net.Conn, args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("empty request")
+	}
+	cmd := args[0]
+	switch strings.ToLower(cmd) {
+	case "ping":
+		conn.Write([]byte("+PONG\r\n")) 
+	case "echo":
+		if len(args) < 2 {
+			return nil
+		}
+		conn.Write([]byte(fmt.Sprintf("+%s\r\n", args[1])))
+	default:
+		return fmt.Errorf("unknown command")
+	}
+	return nil
+}
+
+
+
+	
+
+
+
+
+
